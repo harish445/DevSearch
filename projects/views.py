db6ba6da7953de2eed29d2e69a23e2e32a0787f6
harish <harishthakur4455@gmail.com>
2022-02-26 10:13:41 +0530

    first commit

---
+++ projects/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+projectList = [
+    {
+        'id' : '1',
+        'title': "Ecom website",
+        'description': "fully website"
+    },
+    {
+        'id' : '2',
+        'title': "portfolio website",
+        'description': "fully website"
+    },
+    {
+        'id' : '3',
+        'title': "social website",
+        'description': "fully website"
+    }
+]
+
+def projects(request):
+    page = 'Home'
+    number = 11
+    context = {'page': page, 'number': number, 'projects':projectList}
+    
+    return render(request, 'projects/projects.html', context)
+
+def project(request, pk):
+    projectObj = None
+    for i in projectList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project':projectObj})
