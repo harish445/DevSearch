881bcf2aa7e3bce0688e4a8c338d6b32ff4876a1
harish <harishthakur4455@gmail.com>
2022-03-05 20:17:46 +0530

    login and logout
    design and theme to login and logout page. render messages.

--- users/views.py
+++ users/views.py
@@ -1,13 +1,16 @@
+from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
 def loginUser(request):
+    page = 'login'
     
     if request.user.is_authenticated:
         return redirect('profiles')
@@ -33,8 +36,30 @@ def loginUser(request):
 
 def logoutUser(request):
     logout(request)
-    messages.error(request, "User was logged out")
+    messages.info(request, "User was logged out")
     return redirect('login')
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+    
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            
+            messages.success(request, 'User account was created!')
+            
+            login(request, user)
+            return redirect('profiles')
+        
+        else:
+            messages.success(request, 'Error!')
+    
+    context = {'page':page, 'form': form}
+    return render(request, 'users/login_register.html', context)
 
 def profiles(request):
     profiles = Profile.objects.all()
